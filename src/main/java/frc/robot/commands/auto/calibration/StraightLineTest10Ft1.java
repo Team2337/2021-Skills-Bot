a2521d42bc98aa5106782493d0ebf4c23a75fd5b
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2021-03-01 21:20:29 -0500

    Change calibration folder to be lowercase

---
+++ src/main/java/frc/robot/commands/auto/calibration/StraightLineTest10Ft1.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.auto.calibration;
+
+import java.io.IOException;
+
+import frc.robot.commands.auto.PathweaverTrajectoryCommand;
+import frc.robot.subsystems.SwerveDrivetrain;
+
+public class StraightLineTest10Ft1 extends PathweaverTrajectoryCommand {
+  public StraightLineTest10Ft1(SwerveDrivetrain drivetrain) throws IOException {
+    super("output/StraightLineTest10Ft1.wpilib.json", drivetrain);
+  }
+}
