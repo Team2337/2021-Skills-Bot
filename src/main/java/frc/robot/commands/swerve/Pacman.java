1d030e2828e9c2a5ed14a925f4134f6910e31e3f
M. Francis <58670911+mfrancis33@users.noreply.github.com>
2021-03-30 10:23:19 -0400

    Fixed unclear spacing
    Spacing was uneven and I couldn't read it so I fixed it

--- src/main/java/frc/robot/commands/swerve/Pacman.java
+++ src/main/java/frc/robot/commands/swerve/Pacman.java
@@ -11,39 +11,38 @@ public class Pacman {
 
   private SwerveDrivetrain drivetrain;  //TODO why not used???
 
-    double halfRobotPlus = 1.1; //foot ? probably need meters for all
+  double halfRobotPlus = 1.1; //foot ? probably need meters for all
+
+  /**
+   * @return
+   * 
+   */
+  public void pacmanSlalom(double forward, double strafe, SwerveDrivetrain drivetrain) {
+    this.drivetrain = drivetrain;
+
+    double robotX = drivetrain.getPose().getTranslation().getX();
+    double robotY = drivetrain.getPose().getTranslation().getY();
+
+    if (robotX < 5+halfRobotPlus & robotY < 5+halfRobotPlus & forward < 0) forward = 0;
+
+    if (robotX < 10-halfRobotPlus & strafe < 0) strafe = 0; 
+
+    if (robotX > 10-halfRobotPlus & robotX < 20+halfRobotPlus){
+      if (robotX < 10 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward > 0) forward = 0;
+      if (robotX > 20 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward < 0) forward = 0;
+      if (robotY > 4.5000 & robotY < 5+halfRobotPlus & strafe < 0) strafe = 0;
+      if (robotY < 4.4999 & robotY > 5-halfRobotPlus & strafe > 0) strafe = 0;
+    }
+
+    if (robotX > 19.999+halfRobotPlus & robotX < 25-halfRobotPlus) { /*nothing*/  }
+
+    if (robotX > 25-halfRobotPlus & robotX < 25+halfRobotPlus){
+      if (robotX < 25 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward > 0) forward = 0;
+      if (robotX > 25 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward < 0) forward = 0;
+      if (robotY > 5.0001 & robotY < 5+halfRobotPlus & strafe < 0) strafe = 0;
+      if (robotY < 5.0000 & robotY > 5-halfRobotPlus & strafe > 0) strafe = 0;
+    }
     
-    /**
-     * @return
-     * 
-     */
-    public void pacmanSlalom(double forward, double strafe, SwerveDrivetrain drivetrain) {
-      this.drivetrain = drivetrain;
-
-      double robotX = drivetrain.getPose().getTranslation().getX();
-      double robotY = drivetrain.getPose().getTranslation().getY();
-
-      if (robotX < 5+halfRobotPlus & robotY < 5+halfRobotPlus & forward < 0) forward = 0;
-
-      if (robotX < 10-halfRobotPlus & strafe < 0) strafe = 0; 
-
-      if (robotX > 10-halfRobotPlus & robotX < 20+halfRobotPlus){
-        if (robotX < 10 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward > 0) forward = 0;
-        if (robotX > 20 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward < 0) forward = 0;
-        if (robotY > 4.5000 & robotY < 5+halfRobotPlus & strafe < 0) strafe = 0;
-        if (robotY < 4.4999 & robotY > 5-halfRobotPlus & strafe > 0) strafe = 0;
-        }
-
-      if (robotX > 19.999+halfRobotPlus & robotX < 25-halfRobotPlus) { /*nothing*/  }
-
-      if (robotX > 25-halfRobotPlus & robotX < 25+halfRobotPlus){
-        if (robotX < 25 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward > 0) forward = 0;
-        if (robotX > 25 & robotY > 5-halfRobotPlus & robotY < 5+halfRobotPlus & forward < 0) forward = 0;
-        if (robotY > 5.0001 & robotY < 5+halfRobotPlus & strafe < 0) strafe = 0;
-        if (robotY < 5.0000 & robotY > 5-halfRobotPlus & strafe > 0) strafe = 0;
-        }
-    
-    } //end pacmanSlalom.  probably and else's
-
+  } //end pacmanSlalom.  probably and else's
 
 }
